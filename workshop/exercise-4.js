6605fe8b1d86d4607279d8f6a2a8bdb40cd46d2a
Ning An <ning.an@outlook.com>
2020-06-15 16:24:30 -0400

    Exercise4-solution

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -6,5 +6,8 @@
 // 0, 1, 4, 9, 16, ...
 
 for (let number = 0; number < 12; number++) {
+  let square = number ** 2;
   console.log('the square of ', number, ' is ', square);
 }
+
+//Q: Should 12 be included or now?
