d5b964a041beb5059c8727aafb6fad8e3e90f3df
Ning An <ning.an@outlook.com>
2020-06-15 17:34:43 -0400

    Exercise12-solution

--- workshop/exercise-12-stretch.js
+++ workshop/exercise-12-stretch.js
@@ -12,3 +12,23 @@
 // NOTE: 1 and 2 are both prime numbers.
 
 // EXPECTED OUTPUT: [1, 2, 3, 5, 7, 11, 13, ...]
+
+let myArray = [1, 2, 3];
+
+for (let num=4; num<=200; num++) {
+    let test = false;
+    for (let i=1; i<myArray.length; i++) {
+        if (num % myArray[i] === 0) {
+            test = false;
+            break;
+        } else {
+            test = true;
+        }
+    }
+    
+    if (test) {
+        myArray.push(num);
+    }
+}
+
+console.log(myArray);
