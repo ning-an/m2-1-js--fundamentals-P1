74871492a4d23b036cb09dd54d08291db5035490
Ning An <ning.an@outlook.com>
2020-06-15 16:34:29 -0400

    Exercise6.5-solution

--- workshop/exercise-6.5.js
+++ workshop/exercise-6.5.js
@@ -9,3 +9,11 @@
 // ...
 
 // many other possibilities here.
+
+for (let i=0; i<=15; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is even`);
+    } else {
+        console.log(`${i} is odd`);
+    }
+}
