218b44a7f0ad76b2e0fd0c18573484afea3fd2d3
Ning An <ning.an@outlook.com>
2020-06-15 16:42:27 -0400

    Exercise8-solution

--- workshop/exercise-8.js
+++ workshop/exercise-8.js
@@ -3,3 +3,13 @@
 // between 0 and 5000
 
 // ANSWER: the number you should see in the console is 3127500
+
+let total = 0;
+
+for (let i=0; i<=5000; i++) {
+    if (i % 4 === 0) {
+        total += i;
+    }
+}
+
+console.log(total)
