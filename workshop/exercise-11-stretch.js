be0facec192d5550eb96e5fa442a5b3a0f225192
Ning An <ning.an@outlook.com>
2020-06-15 16:59:51 -0400

    Exercise11-solution

--- workshop/exercise-11-stretch.js
+++ workshop/exercise-11-stretch.js
@@ -11,3 +11,18 @@
 // _#_#_#_#
 // #_#_#_#_
 // _#_#_#_#
+
+let str = '';
+
+for (let i=0; i<8; i++) {
+    for (let j=0; j<8; j++) {
+        if ((i + j) % 2 === 0) {
+            str += '#'
+        } else {
+            str += '_'
+        }
+    }
+    str += '\n'
+}
+
+console.log(str);
